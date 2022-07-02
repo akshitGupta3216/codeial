08b7ecdc040b2958020114636cf0b959604dbf1b
akshitgupta3216 <akshit1.hhc@gmail.com>
2022-07-02 12:43:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8001;
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error in running the server ${err}`);
+    }
+    console.log(`server is running on port ${port}`);
+});
